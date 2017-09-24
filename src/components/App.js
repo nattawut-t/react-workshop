31ae5801cab8633305b66836bfe6ab209116b23d
j2sdk_ball2 <codecrazii@gmail.com>
2017-09-24 22:47:42 +0700

    add form inputs

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,73 @@ import { render } from 'react-dom'
 class App extends Component {
   render() {
     return (
-      <div>App Component</div>
+      <form>
+        <div className="post-preview">
+          <div className="row">
+            <div className="col-4">
+              <h3 className="post-subtitle">
+                Username:
+            </h3>
+            </div>
+            <h2 className="col-8">
+              <input type="text" placeholder="username" />
+            </h2>
+          </div>
+          <div className="clearfix" >&nbsp;</div>
+          <div className="row">
+            <div className="col-4">
+              <h3 className="post-subtitle">
+                First name:
+            </h3>
+            </div>
+            <h2 className="col-8">
+              <input type="text" placeholder="first name" />
+            </h2>
+          </div>
+          <div className="clearfix" >&nbsp;</div>
+          <div className="row">
+            <div className="col-4">
+              <h3 className="post-subtitle">
+                Last name:
+            </h3>
+            </div>
+            <h2 className="col-8">
+              <input type="text" placeholder="last name" />
+            </h2>
+          </div>
+          <div className="clearfix" >&nbsp;</div>
+          <div className="row">
+            <div className="col-4">
+              <h3 className="post-subtitle">
+                Email:
+            </h3>
+            </div>
+            <h2 className="col-8">
+              <input type="text" placeholder="email" />
+            </h2>
+          </div>
+          <div className="clearfix" >&nbsp;</div>
+          <div className="row">
+            <div className="col-4">
+              <h3 className="post-subtitle">
+                Password:
+            </h3>
+            </div>
+            <h2 className="col-8">
+              <input type="text" placeholder="password" />
+            </h2>
+          </div>
+          <div className="clearfix" >&nbsp;</div>
+          <div
+            style={{
+              justifyContent: 'center',
+              display: 'flex',
+            }}
+          >
+            <input type="submit" className="btn btn-secondary" value="Submit" />
+          </div>
+        </div >
+      </form>
     )
   }
 }
